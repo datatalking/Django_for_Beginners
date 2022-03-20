a7a3bc0118fd8f3884b6a0a77aabdfc31cb32aec
Owner <yourdatatalking@gmail.com>
2022-03-20 16:36:20 -0700

    Initial commit - laptop lifestyle

---
+++ README.md.py
@@ -0,0 +1,3 @@
+# Django for Beginners - the potential to earn billions
+
+%.md If data is the new oil then what is the drill, that could be considered anything from a laptop, a smartphone to a warehouse to 
